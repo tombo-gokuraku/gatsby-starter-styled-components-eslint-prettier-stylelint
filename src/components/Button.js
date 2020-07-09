6d4b497ea92ab20db25f80f8b09fc79d027996ce
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-07-09 12:17:08 +0900

    add Button component

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+// ./src/components/Button.js
+import styled from "styled-components"
+
+const Button = styled.button`
+  padding: 4px 16px;
+  margin: 8px;
+  color: black;
+  background-color: lightgreen;
+  border-radius: 8px;
+`
+
+export default Button
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
+// ./src/pages/index.js
 import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+
+import Button from "../components/Button"
 
 const IndexPage = () => (
   <Layout>
@@ -14,6 +17,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: "300px", marginBottom: "1.45rem" }}>
       <Image />
     </div>
+    <Button>hoge</Button>
     <Link to="/page-2/">Go to page 2</Link>
     <Link to="/using-typescript/">Go to &quot;Using TypeScript&quot;</Link>
   </Layout>
